9dd5d39e0a3ad1c7500dc73a433aa1d975290df4
Diego 'Flameeyes' Pettenò <flameeyes@gentoo.org>
2006-08-07 15:05:27 +0200

    Add copyright/license header to conversions.h

--- generator/conversions.h
+++ generator/conversions.h
@@ -1,6 +1,19 @@
-/*
- * kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 4;
- */
+// Flameeyes's C++<->Ruby bindings generator
+// Copyright (C) 2006, Diego "Flameeyes" Pettenò
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this generator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 static inline VALUE cxx2ruby(uint val)
 {
@@ -43,3 +56,5 @@ static inline char *ruby2charPtr(VALUE rval)
     Check_Type(rval, T_STRING);
     return StringValuePtr(rval);
 }
+
+// kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 4;
